aa6109f9f42a93e64e595031e71e812a483d8735
Sorarat Urairaksa <sorarat.urairaksa@gmail.com>
2024-05-25 16:44:21 +0800

    added event box to home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './react.svg'
 import './Home.css'
-import { Flex, Text, Image, Button } from '@chakra-ui/react'
+import EventBox from '../EventBox/EventBox'
+import { Flex } from '@chakra-ui/react'
 
 const Home: React.FC = () => {
-	const [count, setCount] = useState(0)
+	const events = [
+		{
+			id: 1,
+			name: 'Username',
+			date: '2024-05-25',
+			time: '10:00 AM',
+			title: 'Event #1',
+			description: 'Description for event one.',
+		},
+		{
+			id: 2,
+			name: 'Username',
+			date: '2024-05-26',
+			time: '10:00 AM',
+			title: 'Event #2',
+			description: 'Description for event two.',
+		},
+	]
 
 	return (
 		<Flex
@@ -12,21 +28,11 @@ const Home: React.FC = () => {
 			flexDir="column"
 			alignItems="center"
 			paddingTop="40px"
+			gap="20px"
 		>
-			<Flex flexDir="row">
-				<Image
-					src={reactLogo}
-					boxSize="160px"
-					className="logo"
-					id="react-logo"
-				/>
-			</Flex>
-			<Text fontSize="xl" margin="10px 0 20px 0">
-				Test vite hot reloading feature: {count}
-			</Text>
-			<Button onClick={() => setCount((count) => count + 1)}>
-				Click Me
-			</Button>
+			{events.map((event) => (
+				<EventBox key={event.id} event={event} />
+			))}
 		</Flex>
 	)
 }
