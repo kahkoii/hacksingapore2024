3fab287bc6e2c10c16c7904dc3a42e1dd4173c4a
Kah Ho <sweekahho.gmail.com>
2024-05-26 00:35:32 +0800

    add category dropdown

--- src/pages/Events/Events.tsx
+++ src/pages/Events/Events.tsx
@@ -1,5 +1,6 @@
-import { Box, Flex, Text } from '@chakra-ui/react'
+import { Box, Flex, Text, Select, Circle } from '@chakra-ui/react'
 import EventBox from './EventBox'
+import { FaSearch } from 'react-icons/fa'
 import eventsData from '../../../data/events.json'
 
 const Events: React.FC = () => (
@@ -9,6 +10,38 @@ const Events: React.FC = () => (
 				Events
 			</Text>
 		</Box>
+		<Flex
+			flexDir="column"
+			alignItems="center"
+			width="100%"
+			margin="40px 0px 20px"
+		>
+			<Flex
+				flexDir="row"
+				width="100%"
+				maxWidth="800px"
+				padding="8px"
+				gap="12px"
+				alignItems="center"
+			>
+				<Select placeholder="Categories">
+					<option value="animals">Animals</option>
+					<option value="children">Children</option>
+					<option value="elderly">Elderly</option>
+					<option value="environment">Environment</option>
+					<option value="impoverished">Impoverished</option>
+				</Select>
+				<Circle
+					padding="14px"
+					bgColor="#00C2A2"
+					color="white"
+					_hover={{ cursor: 'pointer' }}
+					onClick={() => console.log('search')}
+				>
+					<FaSearch />
+				</Circle>
+			</Flex>
+		</Flex>
 		<Flex
 			flexDir="column"
 			alignItems="center"
