b21547f1ec998073169994d32b0f036bc97ef09a
Kah Ho <sweekahho@gmail.com>
2024-05-26 08:11:27 +0800

    create activities page with mobile styling

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,38 +1,26 @@
-import './Home.css'
-import ActivityBox from '../ActivityBox/ActivityBox'
-import { Flex } from '@chakra-ui/react'
+import { Flex, Box, Text } from '@chakra-ui/react'
+import ActivityBox from './ActivityBox'
+import activitiesJson from '../../../data/activities.json'
 
 const Home: React.FC = () => {
-	const activities = [
-		{
-			id: 1,
-			name: 'Username',
-			date: '2024-05-25',
-			time: '10:00 AM',
-			title: 'Event #1',
-			description: 'Description for event one.',
-		},
-		{
-			id: 2,
-			name: 'Username',
-			date: '2024-05-26',
-			time: '10:00 AM',
-			title: 'Event #2',
-			description: 'Description for event two.',
-		},
-	]
-
 	return (
-		<Flex
-			height="100vh"
-			flexDir="column"
-			alignItems="center"
-			paddingTop="40px"
-			gap="20px"
-		>
-			{activities.map((activity) => (
-				<ActivityBox key={activity.id} activity={activity} />
-			))}
+		<Flex flexDir="column" alignItems="center" marginBottom="100px">
+			<Box
+				height="68px"
+				boxShadow="lg"
+				bgColor="#fff"
+				width="100%"
+				alignContent="center"
+			>
+				<Text fontSize="20px" fontWeight="bold" textAlign="center">
+					Activity Feed
+				</Text>
+			</Box>
+			<Flex flexDir="column" marginTop="20px" gap="16px" padding="0 10px">
+				{activitiesJson.map((activity) => (
+					<ActivityBox key={activity.id} activity={activity} />
+				))}
+			</Flex>
 		</Flex>
 	)
 }
